6dd1fa2c602a50c32c4a2e68be8625b198296b89
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-03-18 09:56:58 +0100

    Employee Service Added

--- src/main/java/com/example/Employees/service/EmployeeService.java
+++ src/main/java/com/example/Employees/service/EmployeeService.java
@@ -1,5 +1,17 @@
 package com.example.Employees.service;
 
-public class EmployeeService {
+import com.example.Employees.model.Employee;
+import com.example.Employees.reprository.EmployeeRepository;
 
+import java.util.List;
+
+public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployess(){
+        return employeeRepository.getEmployees();
+    }
+    public Employee addEmployee(Employee employee){
+        return employeeRepository.addEmployee(employee);
+    }
 }
