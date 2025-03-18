9cf3ded9cad763fe07e8e0b74c49072dfeb3d067
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-03-18 05:07:56 +0100

    first commit

---
+++ src/main/java/com/example/Employees/model/Employee.java
@@ -0,0 +1,11 @@
+package com.example.Employees.model;
+
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private String employeeSalary;
+
+    public Employee (int employeeId, String employeeName){
+
+    }
+}
